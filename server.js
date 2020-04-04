0810a261c148b47034b4b276b2977814da308bcb
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-04-04 14:34:41 +0300

    edit

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ const PORT = process.env.PORT || 3000;
 
 server.use(express.static('./public'));
 
+server.get('/', (request, response) => {
+    response.send('You are doing great');
+});
+
+server.use('*', (request,response) =>{
+    response.send('sorry this page not fond');
+})
 
 
-// server.listen(PORT, () => {
-//     console.log('listening on port', PORT);
-// });
-
-
+server.listen(PORT, () => {
+    console.log('hello there that is my port ', PORT);
+});
